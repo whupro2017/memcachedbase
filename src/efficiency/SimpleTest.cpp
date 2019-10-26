10fccd8f606fdc0d3c0046da99eb059a22f63faa
Michael <lwhaywhu@gmail.com>
2019-10-27 04:16:53 +0800

    "Add simple and unit libmemcached test."

---
+++ src/efficiency/SimpleTest.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Michael on 10/27/19.
+//
+
+#include <iostream>
+#include <memcached.h>
+#include "tracer.h"
+
+int main(int argc, char **argv) {
+    memcached_server_st *servers = memcached_servers_parse("localhost");
+    memcached_st * memc = memcached_create(NULL);
+    memcached_server_push(memc, servers);
+    memcached_server_list_free(servers);
+    memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_BINARY_PROTOCOL, (uint64_t) 0);
+    char key[1024];
+    char value[1024];
+    std::memset(key, 0, 1024);
+    std::memset(value, 0, 1024);
+    std::memcpy(value, "hello", 5);
+    Tracer tracer;
+    tracer.startTime();
+    for (int i = 0; i < 10000000; i++) {
+        std::sprintf(key, "%d", i);
+        std::sprintf(value + 5, "%d", i);
+        memcached_add(memc, key, sizeof(key), value, sizeof(value), 0, 0);
+    }
+    cout << tracer.getRunTime();
+    memcached_free(memc);
+    return 0;
+}
---
+++ src/unittests/AddGetDeletTest.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Michael on 10/27/19.
+//
+
+#include <gtest/gtest.h>
+#include <memcached.h>
+
+memcached_server_st *servers = memcached_servers_parse("localhost");
+memcached_st *memc = memcached_create(NULL);
+
+void SetUp() {
+    memcached_server_push(memc, servers);
+    memcached_server_list_free(servers);
+    memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_BINARY_PROTOCOL, (uint64_t) 0);
+}
+
+void TearDown() {
+    memcached_free(memc);
+}
+
+TEST(LibmemcachedTest, AddGet) {
+    memcached_add(memc, "hello", 5, "hello world", 11, 0, 0);
+    size_t value_length = 64;
+    uint32_t flag = 0;
+    memcached_return_t ret;
+    char *value = memcached_get(memc, "hello", 5, &value_length, &flag, &ret);
+    ASSERT_STRCASEEQ("hello world", value);
+    TearDown();
+}
+
+int main(int argc, char **argv) {
+    SetUp();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
