3910c6fa2850b1bcd6833cb4256046a066c060ee
lwhay <lwhay@whu.edu.cn>
2020-02-24 14:05:02 +0800

    enable remote ip

--- test/efficiency/MultiTest.cpp
+++ test/efficiency/MultiTest.cpp
@@ -8,37 +8,57 @@
 #include <memcached.h>
 #include "tracer.h"
 
+#define DEFAULT_KEY_LENGTH (1 << 6)
+
 uint64_t total_count = (1 << 24);
 
 int thread_number = 2;
 
-memcached_server_st *servers = memcached_servers_parse("localhost");
-memcached_st *memc = memcached_create(NULL);
+char *host_ip = "localhost";
+
+memcached_server_st *servers;
+memcached_st *memc;
+
+size_t *runtimes, *msucc, *mfail;
+std::atomic<size_t> modification_success{0};
+std::atomic<size_t> modification_failure{0};
 
 void *measureWorker(void *args) {
     int tid = *(int *) args;
-    char key[64];
-    char value[64];
+    char key[DEFAULT_KEY_LENGTH];
+    char value[DEFAULT_KEY_LENGTH];
     Tracer tracer;
     tracer.startTime();
     memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_BINARY_PROTOCOL, (uint64_t) tid);
-    std::cout << "\tTid" << tid << ": " << tracer.getRunTime() << std::endl;
+    //std::cout << "\tTid" << tid << ": " << tracer.getRunTime() << std::endl;
     for (int i = tid; i < total_count; i += thread_number) {
-        std::memset(key, 0, 64);
-        std::memset(value, 0, 64);
+        std::memset(key, 0, DEFAULT_KEY_LENGTH);
+        std::memset(value, 0, DEFAULT_KEY_LENGTH);
         std::sprintf(key, "%d", i);
         std::memcpy(value, "hello", 5);
         std::sprintf(value + 5, "%d", i);
         memcached_return_t ret = memcached_set(memc, key, std::strlen(key), value, std::strlen(value), 0, 0);
-        if (tid == 0 && (i % 100000 == 0)) cout << "\t" << tid << " " << i << " " << ret << endl;
+        //if (tid == 0 && (i % 100000 == 0)) cout << "\t" << tid << " " << i << " " << ret << endl;
+        if (ret == memcached_return_t::MEMCACHED_SUCCESS) msucc[tid]++;
+        else mfail[tid]++;
     }
+    modification_success.fetch_add(msucc[tid]);
+    modification_failure.fetch_add(mfail[tid]);
+    runtimes[tid] += tracer.getRunTime();
 }
 
 int main(int argc, char **argv) {
-    if (argc > 2) {
-        total_count = std::atol(argv[1]);
-        thread_number = std::atoi(argv[2]);
+    if (argc > 3) {
+        thread_number = std::atoi(argv[1]);
+        total_count = std::atol(argv[2]);
+        host_ip = argv[3];
     }
+    cout << "thread: " << thread_number << " " << total_count << " " << host_ip << endl;
+    servers = memcached_servers_parse(host_ip);
+    runtimes = new size_t[thread_number];
+    msucc = new size_t[thread_number];
+    mfail = new size_t[thread_number];
+    memc = memcached_create(NULL);
     memcached_server_push(memc, servers);
     //memcached_server_list_free(servers);
     Tracer tracer;
@@ -47,12 +67,24 @@ int main(int argc, char **argv) {
     int tids[thread_number];
     for (int i = 0; i < thread_number; i++) {
         tids[i] = i;
+        msucc[i] = 0;
+        mfail[i] = 0;
         pthread_create(&threads[i], nullptr, measureWorker, tids + i);
     }
     for (int i = 0; i < thread_number; i++) {
         pthread_join(threads[i], nullptr);
     }
-    cout << total_count << "<->" << tracer.getRunTime() << endl;
+
+    size_t total_runtime = 0;
+    for (int i = 0; i < thread_number; i++) {
+        cout << "\t" << i << " " << runtimes[i] << " " << msucc[i] << " " << mfail[i] << endl;
+        total_runtime += runtimes[i];
+    }
+    cout << total_count << "<->" << tracer.getRunTime() << " " << modification_success << " " << modification_failure
+         << " " << ((double) modification_success + modification_failure) * thread_number / total_runtime << endl;
     memcached_free(memc);
+    delete[] runtimes;
+    delete[] msucc;
+    delete[] mfail;
     return 0;
 }
